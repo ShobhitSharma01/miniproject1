fcb922beb17f7a8c5cdb1b6709c96f0c067401f1
Your Name <your_email@example.com>
2025-05-21 18:05:44 +0530

    intial commit

--- src/Alltabs.jsx
+++ src/Alltabs.jsx
@@ -1,50 +1,47 @@
-import React from 'react'
-import Counter from './counter/Counter'
-import Todolist from './todolist/Todolist'
-import Calculator from './calculator/Calculator'
-// import { useState } from 'react'
-import { BrowserRouter as Router,Routes,Route, Link, useNavigate } from 'react-router-dom'
- const Alltabs = () => {
-  const navigate=useNavigate();
+import React from "react";
+import Counter from "./counter/Counter";
+import Todolist from "./todolist/Todolist";
+import Calculator from "./calculator/Calculator";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+const Alltabs = () => {
+  const navigate = useNavigate();
 
-  const  handleClick=()=>{
+  const handleClick = () => {
     navigate("/counter");
   };
-  const handleSubmit=()=>{
-    navigate("/todolist")
-  }
-  const handleChange=()=>{
-    navigate("/calculator")
-  }
+  const handleSubmit = () => {
+    navigate("/todolist");
+  };
+  const handleChange = () => {
+    navigate("/calculator");
+  };
   return (
-    <div className='main container'>      
-      <div className=' button container'>
-        {/* <li><Link to="/counter">Counter</Link></li>
-        <li><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/todolist">TodoList</Link></li> */}
-<button  onClick={(handleClick)}>Counter</button>
-<button onClick={(handleSubmit)}>Todo App</button>
-<button onClick={(handleChange)}>calculator</button>
+    <div className="main container">
+      <div className=" button container">
+        <button onClick={handleClick}>Counter</button>
+        <button onClick={handleSubmit}>Todo App</button>
+        <button onClick={handleChange}>calculator</button>
+      </div>
+      <div className="render container">
+        <Routes>
+          <Route path="/Counter" element={<Counter />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/todolist" element={<Todolist />} />
+        </Routes>
+      </div>
+      <div className="footer">
+        <button onClick={handleSubmit}>Go to Todo app</button>
+        <button onClick={handleChange}>Go to Calculator App</button>
+        <button onClick={handleClick}>Go to Counter</button>
+      </div>
+    </div>
+  );
+};
 
-</div>
-<div className='render container'>
-     <Routes>
-    <Route path="/Counter" element={<Counter/>}/>
-      <Route path="/calculator" element={<Calculator/>}/>
-       <Route path='/todolist' element={<Todolist/>}/>
-      </Routes>
-
-{/* {page==="todo"&& <Todolist/>}
-{page==="counter" &&<Counter/>}
-{page==="calculator" && <Calculator/>} */}
-</div>
-<div className='footer'>
-<button onClick={(handleSubmit)}>Go to Todo app</button>
-<button onClick={(handleChange)}>Go to Calculator App</button>
-<button onClick={(handleClick)}>Go to Counter</button>
-</div>
-</div>
-)
-}
-
-export  default Alltabs;
+export default Alltabs;
--- src/todolist/Todolist.jsx
+++ src/todolist/Todolist.jsx
@@ -1,84 +1,55 @@
-import React, { useState } from 'react'
-import "./todo.css"
-//import { list } from 'postcss';
-//  const Todolist = () => {
-//   let [todolist,setTodolist]=useState([]);
-//   let saveToDolist=(event)=>{
-//     event.preventDefault();
-//      let toname=event.target.toname.value;
-//      if(!todolist.includes(toname)){
-//       let finalDolist=[...todolist,toname]
-//       setTodolist(finalDolist);
-        
-//      }
-//      else{
-//       alert ("ToDo already add");
-//      }
-   
-//   }
-//   return (
-//     <div className='App'>
-//       <h1>Todo app</h1>
-//       <form onSubmit={saveToDolist}>
-//         <input type="Todo"  name="toname"/>
-//         <button>Add Todo</button>
-//         </form> 
-//     </div>
-//   )
-// }
+import React, { useState } from "react";
+import "./todo.css";
+const Todolist = () => {
+  const [todolist, setTodolist] = useState([]);
 
-// export default Todolist;
-const Todolist=()=>{
-const [todolist,setTodolist]=useState([]);
+  let saveTodoList = (event) => {
+    event.preventDefault();
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let finalDolist = [...todolist, toname];
+      setTodolist(finalDolist);
+    } else {
+      alert("already add!");
+    }
+  };
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDolistItem
+        value={value}
+        key={index}
+        indexNumber={index}
+        todolist={todolist}
+        setTodolist={setTodolist}
+      />
+    );
+  });
 
-
-
-let saveTodoList=(event)=>{
-  event.preventDefault();
-  let toname=event.target.toname.value;
-  if(!todolist.includes(toname)){
-    let finalDolist=[...todolist,toname]
-    setTodolist(finalDolist);
-  }
-
-else{
-  alert("already add!");
-}
-}
-let list=todolist.map((value,index)=>{
-  return(
-    <ToDolistItem value={value} key={index} indexNumber={index}
-    todolist={todolist}
-    setTodolist={setTodolist}
-    />
-  )
-})
-
-return(
-  <div className="App">
-    <h1>Todo app</h1>
-    <form onSubmit={saveTodoList}>
-    <input type="text"  name="toname"/>
-    <button>Add</button>
-    </form>
-    <div className="outerDiv">
-    <ul>
-
-{list}
- 
-    </ul>
+  return (
+    <div className="App">
+      <h1>Todo app</h1>
+      <form onSubmit={saveTodoList}>
+        <input type="text" name="toname" />
+        <button>Add</button>
+      </form>
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
     </div>
-  </div>
-);
-}
+  );
+};
 export default Todolist;
 
-function ToDolistItem({value,indexNumber,todolist,setTodolist}){
- let deleteRow=()=>{
-  let finalData=todolist.filter((v,i)=>i!=indexNumber)
-  setTodolist(finalData)
- }
-  return(
-    <li> {indexNumber}{value} <span onClick={deleteRow}>&times;</span></li>
-  )
+function ToDolistItem({ value, indexNumber, todolist, setTodolist }) {
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i != indexNumber);
+    setTodolist(finalData);
+  };
+  return (
+    <li>
+      {" "}
+      {indexNumber}
+      {value} <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
 }
